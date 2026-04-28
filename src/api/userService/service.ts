ead5d96ff7765c27736a6ba3b9f6c3a859a82e3a
francisaragdon <70627670+francisaragdon@users.noreply.github.com>
2026-04-29 01:33:43 +0800

    update .env

--- src/api/userService/service.ts
+++ src/api/userService/service.ts
@@ -1,11 +1,12 @@
 import { APIRequestContext } from "@playwright/test";
 
 export class UserService {
-    constructor(private request: APIRequestContext) {}
+    constructor(private request: APIRequestContext) { }
 
-    async postUser(payload: any){
+    async postUser(payload: any) {
         return await this.request.post('/api/users', {
-             data: payload });
+            data: payload
+        });
     }
 
 }
